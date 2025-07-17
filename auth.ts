e38e4fe566c4c6f3527575b7e73a874c441013e9
haydarih833 <haydarih833@gmail.com>
2025-07-17 19:16:02 +0330

    convert url default

--- auth.ts
+++ auth.ts
@@ -16,7 +16,7 @@ const authConfig: NextAuthConfig = {
       async authorize(credentials, request) {
         const { email, password } = credentials as SignInCredentials;
         // send request to your api route where you can sign in you user and send error or success response to this function.
-        const { user, error } = await fetch("https://nextjs-shop-nu-olive.vercel.app/api/users/singin"!, {
+        const { user, error } = await fetch(`${process.env.API_SIGN_IN_ENDPOINT}/api/users/signin`, {
           method: "POST",
           body: JSON.stringify({ email, password }),
         }).then(async (res) => await res.json());
